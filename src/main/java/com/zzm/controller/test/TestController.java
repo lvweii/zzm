9b7eb8405211c16e54bf79dc83f44bf36889bbea
lvwei <654792577@qq.com>
2018-09-11 21:56:39 +0800

    文件上传

--- src/main/java/com/zzm/controller/test/TestController.java
+++ src/main/java/com/zzm/controller/test/TestController.java
@@ -2,17 +2,25 @@ package com.zzm.controller.test;
 
 import com.zzm.domain.User;
 import com.zzm.service.testService.TestService;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -81,4 +89,88 @@ public class TestController {
     session.setAttribute("msg",s);
     return "test";
   }
+
+  /**
+   *  保存文档到服务器磁盘，返回值true，保存成功，返回值为false时，保存失败。
+   */
+  @RequestMapping(value = {"/testUpload.action"}, method = {RequestMethod.GET,RequestMethod.POST})
+  @ResponseBody
+  public boolean saveFileToDisk(HttpServletRequest request,HttpServletResponse response)
+  {
+    File officeFileUpload = null;
+    FileItem officeFileItem = null;
+    String officefileNameDisk = new Date().getTime()+".docx";
+    String absoluteOfficeFileDir = "C:\\Users\\Lv\\Desktop\\upload\\office";
+    boolean result=true ;
+
+    DiskFileItemFactory factory = new DiskFileItemFactory();
+    // 设置最多只允许在内存中存储的数据,单位:字节
+    factory.setSizeThreshold(4096);
+    // 设置一旦文件大小超过setSizeThreshold()的值时数据存放在硬盘的目录
+//    factory.setRepository(new File(tempFileDir));
+    ServletFileUpload upload = new ServletFileUpload(factory);
+    //设置允许用户上传文件大小,单位:字节
+    upload.setSizeMax(1024*1024*4);
+    List fileItems = null;
+//    Iterator<String> iterator = req.getFileNames();
+//    while (iterator.hasNext()) {
+//      MultipartFile file = req.getFile(iterator.next());
+//      String fileNames = file.getOriginalFilename();
+//      int split = fileNames.lastIndexOf(".");
+//      //存储文件
+//      //文件名
+//      System.out.println(fileNames.substring(0,split));
+//      //文件格式
+//      System.out.println(fileNames.substring(split+1,fileNames.length()));
+//      //文件内容
+////      System.out.println(file.getBytes());
+//    }
+    try{fileItems=upload.parseRequest(request);}
+    catch(FileUploadException e)
+    {
+      System.out.println("the max upload size is 4m,cheeck upload file size!");
+      System.out.println(e.getMessage());
+      e.printStackTrace();
+      return false;
+    }
+    Iterator iter = fileItems.iterator();
+    while (iter.hasNext())
+    {
+      FileItem item = (FileItem) iter.next();
+      //打印提交的文本域和文件域名称
+      System.out.println(item.getFieldName());
+      if(item.isFormField())
+      {
+//普通文本表单字段
+//        if(item.getFieldName().equalsIgnoreCase("fileType"))
+//        {
+//          fileType=item.getString("utf-8").trim();
+//        }
+      }else
+      {
+//        if(item.getFieldName().equalsIgnoreCase("upLoadFile"))
+//        {officeFileItem=item;}
+        officeFileItem=item;
+      }
+    }
+
+    try
+    {
+      System.out.println("officefilepath:"+absoluteOfficeFileDir+officefileNameDisk);
+      if(!officefileNameDisk.equalsIgnoreCase("")&&officeFileItem!=null)
+      {
+        officeFileUpload =  new File(absoluteOfficeFileDir+officefileNameDisk);
+        officeFileItem.write(officeFileUpload);
+      }
+    }
+    catch(FileNotFoundException e){}
+    catch(Exception e)
+    {
+      System.out.println("error saveFileToDisk:"+e.getMessage());
+      e.printStackTrace();
+      result=false;
+    }
+    return result;
+  }
+
 }
---
+++ src/main/webapp/jsp/upload.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Lv
+  Date: 2018/9/11
+  Time: 21:29
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="/testUpload.action" enctype="multipart/form-data"
+      method="post">
+    上传用户：<input type="text" name="username"/><br/>
+    上传文件：<input type="file" name="file"/><br/>
+    <input type="submit" value="提交"/>
+</form>
+</body>
+</html>
